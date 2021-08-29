c5ae3e79b51133192b368c3f8a48b9d4cb8b25fe
PhilwithoneL <philip.r.fielding@gmail.com>
2021-08-29 11:31:55 +0100

    V0.04 Welcome page Creation
    - Added parallax scroll with images
    
    - Added business name to Navigation bar
    
    - Added welcome message
    
    Installed Aos for future scroll animations

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, {useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Logo from '../assets/ambrosia-logo.svg';
 import M from 'materialize-css/dist/js/materialize.min.js'
 
@@ -16,6 +16,7 @@ export const NavBar = () => {
         <div>
           <nav>
             <div class="nav-wrapper white">
+            <a href="#!" class="nav-logo-name brand-logo grey-text text-darken-4 "><h4>Ambrosia Kitchens</h4></a>
               <a href="#!" class="brand-logo center"><img class = "nav-logo" src={Logo} alt="Ambrosia Logo"/></a>
               <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons nav-side-bar-trigger">menu</i></a>
               <ul class="right hide-on-med-and-down">
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import NavBar from './NavBar';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import kitchenWelcomeImg1 from '../assets/Welcome-Kitchen.jpg'
+import kitchenWelcomeImg2 from '../assets/Welcome-Kitchen2.jpg'
 
-const Welcome = () => {
+export const Welcome = () => {
+
+    useEffect(() => {
+        M.AutoInit();
+        
+
+    },[]) 
+
     return (
         <div>
             <NavBar />
-
+            <div class="parallax-container">
+                <div class="parallax"><img src={ kitchenWelcomeImg1 }/></div>
+              </div>
+              <div class="section white welcome-message">
+                <div class="row container">
+                  <h2 class="header">Welcome!</h2>
+                  <p class="grey-text text-darken-3 lighten-3"> 
+                    <p>We offer high quality, bespoke kitchen renovations in the Manchester area. We offer a range styles and designs to best meet your needs and budget.</p>
+                    
+                    <div className = "quote">Click the link below to get a quote or stop round our showroom today!</div>
+                  </p>
+                </div>
+              </div>
+              <div class="parallax-container">
+                <div class="parallax"><img src={ kitchenWelcomeImg2 }/></div>
+              </div>
         </div>
     )
 }
