7a867e53a7a5bf71666cda302704b7cb61eff946
PhilwithoneL <philip.r.fielding@gmail.com>
2021-09-04 14:52:59 +0100

    V0.13 Contact Page creation
    - Embedded google map for address

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,19 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
-            
 
+        
+
+            <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6714.367646314672!2d-2.2930152135206874!3d53.48834083889077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487bae3717085017%3A0x321cc424fa329ca2!2sSalford%20Shopping%20Centre!5e0!3m2!1sen!2suk!4v1630763119182!5m2!1sen!2suk" 
+                width="800" 
+                height="800" 
+                style={{border:0}}
+                allowfullscreen="" 
+                loading="lazy">
+
+            </iframe>
+        
         </div>
     )
 }
--- src/components/OurWork.js
+++ src/components/OurWork.js
@@ -1,10 +1,10 @@
 import { React, useEffect } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import HomeBtn from './HomeBtn';
-import kitchenAboutImg1 from '../assets/about-kitchen1.jpg';
-import kitchenAboutImg2 from '../assets/about-kitchen2.jpg';
-import kitchenWelcomeImg1 from '../assets/Welcome-Kitchen.jpg';
-import kitchenWelcomeImg2 from '../assets/Welcome-Kitchen2.jpg';
+import Example1 from '../assets/Example1.jpg';
+import Example2 from '../assets/Example2.jpg';
+import Example3 from '../assets/Example3.jpg';
+import Example4 from '../assets/Example4.jpg';
 
 const OurWork = () => {
 
@@ -21,11 +21,11 @@ const OurWork = () => {
 
             <div className = "row">
 
-            <div className = "col m6 s12">
+            <div className = "col m6 s12 l6 xl3">
 
                 <div class="card large">
                     <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src= { kitchenAboutImg1 }  />
+                    <img class="activator" src= { Example1 }  />
                     </div>
                     <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">Bright, Open and Natural<i class="material-icons right">more_vert</i></span>
@@ -33,17 +33,17 @@ const OurWork = () => {
                     </div>
                     <div class="card-reveal">
                     <span class="card-title grey-text text-darken-4">Bright, Open and Natural<i class="material-icons right">close</i></span>
-                    <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                    <p>This is an example of where luck meets opp</p>
                     </div>
                 </div>
 
             </div>
 
-            <div className = "col m6 s12">
+            <div className = "col m6 s12 l6 xl3">
 
                 <div class="card large">
                     <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src= { kitchenAboutImg2 }  />
+                    <img class="activator" src= { Example2 }  />
                     </div>
                     <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">Sleek and Sophisticated<i class="material-icons right">more_vert</i></span>
@@ -57,11 +57,11 @@ const OurWork = () => {
 
             </div>
 
-            <div className = "col m6 s12">
+            <div className = "col m6 s12 l6 xl3">
 
                 <div class="card large">
                     <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src= { kitchenWelcomeImg1 }  />
+                    <img class="activator" src= { Example3 }  />
                     </div>
                     <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">Stylish and Trendy<i class="material-icons right">more_vert</i></span>
@@ -75,11 +75,11 @@ const OurWork = () => {
 
             </div>
 
-            <div className = "col m6 s12">
+            <div className = "col m6 s12 l6 xl3">
 
                 <div class="card large">
                     <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src= { kitchenWelcomeImg2 }  />
+                    <img class="activator" src= { Example4 }  />
                     </div>
                     <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">Modern and Simple<i class="material-icons right">more_vert</i></span>
@@ -94,7 +94,7 @@ const OurWork = () => {
             </div>
 
             </div>
-
+            <div className = "section work-footer-divider"></div>
             <HomeBtn />
 
         </div>
