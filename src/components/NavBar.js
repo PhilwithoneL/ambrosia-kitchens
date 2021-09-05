1edbe575535721cf95633ee92106ef86aad06fa5
PhilwithoneL <philip.r.fielding@gmail.com>
2021-09-05 12:07:13 +0100

    V0.18 Navigation amendments
    - Added name and logo to pop-up navbar
    
    - Centered the links
    
    - Added the welcome page the navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,11 +15,11 @@ export const NavBar = () => {
         <div>
           <nav>
             <div class="nav-wrapper white">
-            <a href="/" class="nav-logo-name brand-logo grey-text text-darken-4 "><h4>Ambrosia Kitchens</h4></a>
+              <a href="/" class="nav-logo-name brand-logo grey-text text-darken-4 "><h4>Ambrosia Kitchens</h4></a>
               <a href="/" class="brand-logo center"><img class = "nav-logo" src={Logo} alt="Ambrosia Logo"/></a>
               <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons nav-side-bar-trigger">menu</i></a>
               <ul class="right hide-on-med-and-down">
-
+              <li><a href="/about" className = "grey-text text-darken-4">Welcome</a></li>
                 <li><a href="/about" className = "grey-text text-darken-4">About</a></li>
                 <li><a href="/OurWork" className = "grey-text text-darken-4">Our Work</a></li>
                 <li><a href="/Contact" className = "grey-text text-darken-4">Contact</a></li>
@@ -29,10 +29,15 @@ export const NavBar = () => {
           </nav>
 
           <ul class="sidenav " id="mobile-demo">
-            <li><a href="/">Welcome</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/OurWork">Work</a></li>
-            <li><a href="/Contact">Contact</a></li>
+
+            <a href="/" class="nav-logo-name brand-logo grey-text text-darken-4 text-center"><h4>Ambrosia Kitchens</h4></a>
+            <a href="/" class="brand-logo center-div nav-pop-logo"><img class = "nav-logo" src={Logo} alt="Ambrosia Logo"/></a>
+
+            <li className="text-center nav-pop-link"><a href="/">Welcome</a></li>
+            <li className="text-center nav-pop-link"><a href="/about">About</a></li>
+            <li className="text-center nav-pop-link"><a href="/OurWork">Work</a></li>
+            <li className="text-center nav-pop-link"><a href="/Contact">Contact</a></li>
+
           </ul>
             
         </div>
